c3d685721eaf79dbea3fa2443ef4149489120192
M-CHENNA <124498993+M-CHENNA@users.noreply.github.com>
2024-12-19 00:05:56 +0100

    Mise à jour des ports pour l'API sur 5003

--- ml_app/test_predict.py
+++ ml_app/test_predict.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://localhost:5000/predict"
+url = "http://localhost:5003/predict"
 data = {"features": [1.5, 2.3, 3.7, 0.8]}  # Exemple avec 4 valeurs
 
 response = requests.post(url, json=data)
