551a1a53b3c0d9d836c50e034d2e319205a0bcff
M-CHENNA <124498993+M-CHENNA@users.noreply.github.com>
2024-12-19 00:25:49 +0100

    Corriger le test et le workflow CI/CD

--- ml_app/test_predict.py
+++ ml_app/test_predict.py
@@ -1,9 +1,8 @@
 import requests
 
-url = "http://localhost:5003/predict"
-data = {"features": [1.5, 2.3, 3.7, 0.8]}  # Exemple avec 4 valeurs
-
-response = requests.post(url, json=data)
-
-print("Statut:", response.status_code)
-print("Réponse:", response.json())
+def test_prediction():
+    url = "http://localhost:5003/predict"
+    data = {"features": [5.1, 3.5, 1.4, 0.2]}
+    response = requests.post(url, json=data)
+    assert response.status_code == 200
+    assert response.json().get("prediction") == "setosa"
