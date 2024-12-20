0d21e89f19b5b5ce6c22ebb510f778c980e06a75
M-CHENNA <124498993+M-CHENNA@users.noreply.github.com>
2024-12-20 22:23:00 +0100

    Mise à jour des fichiers

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ features = st.text_input("Entrez les caractéristiques (ex: 5.1, 3.5, 1.4, 0.2)"
 
 if st.button("Prédire"):
     data = {"features": [float(x) for x in features.split(",")]}
-    response = requests.post("http://localhost:5000/predict", json=data)
+    response = requests.post("http://localhost:5003/predict", json=data)
     if response.status_code == 200:
         st.write("Prédiction :", response.json()['prediction'])
     else:
